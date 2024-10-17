1805d2a9f03157060f2818e6aff0c848962e45b2
Jhoncarnack <jhoncarloss2@outlook.com>
2024-10-17 20:30:23 -0300

    configuração do script do jest

--- soma.test.js
+++ soma.test.js
@@ -1,6 +1,6 @@
 const soma = require ('./soma')
 
-test('Deve verificar o resulatdo de uma soma', () => {
+test('Deve verificar o resultado de uma soma', () => {
     expect(soma(3, 8)).toBe(11)
 })
 
